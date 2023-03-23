77da9c390a99f4f5ec13c18699656973470f509d
barakisbrown <lokislayer@gmail.com>
2023-03-22 22:05:11 -0500

    First Commit -- Create a game class.

---
+++ Game/Game/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Game
+    {
+        const int ROWS = 3;
+        const int COLS = 3;
+    }
+}
