a1f8600bf6375907e603d700fdf9a4c13f8c60d8
barakisbrown <lokislayer@gmail.com>
2023-03-25 02:21:48 -0500

    Added Experimental Dictionary Class

---
+++ Game/Game/Symbols.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Symbols
+    {
+        private Dictionary<char, int> SymbolCount = new();
+        private Dictionary<char, int> SymbolValues = new();
+
+        public Symbols()
+        {
+            loadSymbolCount();
+            loadSymbolValues();
+        }
+
+        private void loadSymbolValues()
+        {
+            SymbolValues.Add('A', 5);
+            SymbolValues.Add('B', 4);
+            SymbolValues.Add('C', 3);
+            SymbolValues.Add('D', 2);
+        }
+
+        private void loadSymbolCount()
+        {
+            SymbolCount.Add('A', 2);
+            SymbolCount.Add('B', 4);
+            SymbolCount.Add('C', 6);
+            SymbolCount.Add('D', 8);
+        }
+
+        public Dictionary<char, int> getSymbolCount => SymbolCount;
+        public Dictionary<char, int> getSymbolValues => SymbolValues;
+    }
+}
